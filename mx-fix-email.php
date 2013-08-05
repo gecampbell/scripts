deb43b5eb90df6bc8223a50ebedb03737fd8522d
Glen Campbell <glen@broadpool.com>
2013-08-05 10:39:10 -0500

    more MX/DNS scripts

---
+++ mx-fix-email.php
@@ -0,0 +1,33 @@
+<?php
+// (c)2012 Rackspace Hosting
+// See COPYING for licensing information
+
+// This script generates the /etc/mail/local-host-names file for all
+// domains that match the specified MX_HOST. I've found that maintaining
+// that file manually is a big pain, so I made this to automatically
+// generate it for my mail server.
+
+require_once "php-opencloud.php";
+
+define('AUTHURL', RACKSPACE_US);
+define('USERNAME', $_ENV['OS_USERNAME']);
+define('TENANT', $_ENV['OS_TENANT_NAME']);
+define('APIKEY', $_ENV['NOVA_API_KEY']);
+
+// uncomment for debug output
+//setDebug(TRUE);
+
+// establish our credentials
+$cloud = new \OpenCloud\Rackspace(AUTHURL,
+	array( 'username' => USERNAME,
+		   'apiKey' => APIKEY ));
+
+$dns = $cloud->DNS();
+
+// list all domains
+$dlist = $dns->DomainList();
+while($domain = $dlist->Next()) {
+	printf("%s\n", $domain->Name());
+	$domain->emailAddress = 'glen@glenc.io';
+	$domain->Update();
+}
